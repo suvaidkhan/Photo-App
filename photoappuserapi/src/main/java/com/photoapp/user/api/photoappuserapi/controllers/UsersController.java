6b85772a4ad88195631260fc8bf3c83b193174df
suvaid.khan <suvaid.khan@globallogic.com>
2022-06-06 00:31:29 +0530

    first commit

---
+++ photoappuserapi/src/main/java/com/photoapp/user/api/photoappuserapi/controllers/UsersController.java
@@ -0,0 +1,17 @@
+package com.photoapp.user.api.photoappuserapi.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @GetMapping("/status/check")
+    public String getUser() {
+        return "working fine";
+    }
+
+}
---
+++ photoappuserapi/src/test/java/com/photoapp/user/api/photoappuserapi/PhotoappuserapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoapp.user.api.photoappuserapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappuserapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
